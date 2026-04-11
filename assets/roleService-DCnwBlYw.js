093a6b66c9da38d205a18538447ac9901656cb93
kimngan206 <kimnganlqdqt@gmail.com>
2026-04-11 15:06:05 +0000

    Deploying to gh-pages from @ BienHoaGym/BienHoaGym_FrontEnd-Management@7dcbe4da8e7ef6bed8b34756ac947cc928c95db0 🚀

--- assets/roleService-DCnwBlYw.js
+++
@@ -1,1 +0,0 @@
-import{i as e}from"./router-DVfAUPZl.js";var t={async getAll(){return(await e.get(`/Roles`)).data},async getById(t){return(await e.get(`/Roles/${t}`)).data},async create(t){return(await e.post(`/Roles`,t)).data},async update(t,n){return(await e.put(`/Roles/${t}`,n)).data},async delete(t){return(await e.delete(`/Roles/${t}`)).data}};export{t};
